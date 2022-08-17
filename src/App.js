7ab7868816d59eb70839eab78dd162e92e8ec873
Marco Crupi <marcocrupi@hotmail.it>
2022-08-17 12:51:04 +0200

    add link home about

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    // <BrowserRouter> ci connette al browser
+    <BrowserRouter>
+      {/* <Routes> è parente di tutti i <Route> */}
+      <Routes>
+        {/* <Route> è per ogni pagina ed ha due props path e element, 
+        Nella fase di produzione / rappresenta il dominio del sito */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="products" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
